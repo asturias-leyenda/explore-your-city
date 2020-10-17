7734a8ae1663a6396addd6b5ce227eac9190b62d
Malte Schneider <schneidermalte49@gmail.com>
2020-10-17 11:14:48 +0200

    Register Component

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-
-    Link
-  } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Home() {
     return(
--- src/Register.js
+++ src/Register.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const Form = styled("register-form")`
+
+`;
 
 function Register() {
     return(
         <div>
             <h1>Register</h1>
+            <form id="register-form">
+                <label for="user-name">User Name</label><br/>
+                <input type="text" id="user-name" name="user-name" /><br/>
+                <label for="first-name">First Name</label><br/>
+                <input type="text" id="first-name" name="first-name" /><br/>
+                <label for="last-name">Last Name:</label><br/>
+                <input type="text" id="last-name" name="last-name" />
+                <button >Next</button>
+            </form>
+
             <Link to="/">
-                    <button>enter</button>
+                <button>enter</button>
             </Link>
         </div>
     )
