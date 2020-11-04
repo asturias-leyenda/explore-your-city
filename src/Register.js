d509672f56f8ef3c0a4ec0aa184b98668de35eaf
asturias-leyenda <schneidermalte49@gmail.com>
2020-11-04 22:39:08 +0100

    Register

--- src/Register.js
+++ src/Register.js
@@ -2,26 +2,44 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 
-const Form = styled("register-form")`
-
+const Form = styled.form`
 `;
-
+const Input = styled.input`
+    margin: 5px 30px 20px;
+    padding: 5px;
+    background-color: rgb(230, 230, 230);
+    border-radius: 5px;
+    border: 1px solid grey;
+`;
+const Label = styled.label`
+    margin-bottom: 0px;
+`;
+const Button = styled.button`
+    margin: 10px;
+    padding: 5px;
+    background-color: lightgreen;
+    border-radius: 5px;
+    border: 1px solid grey;
+    width: 40%;
+`;
 function Register() {
     return(
         <div>
             <h1>Register</h1>
-            <form id="register-form">
-                <label for="user-name">User Name</label><br/>
-                <input type="text" id="user-name" name="user-name" /><br/>
-                <label for="first-name">First Name</label><br/>
-                <input type="text" id="first-name" name="first-name" /><br/>
-                <label for="last-name">Last Name:</label><br/>
-                <input type="text" id="last-name" name="last-name" />
-                <button >Next</button>
-            </form>
+            <Form id="register-form">
+                <Label for="user-name">User Name</Label><br/>
+                <Input type="text" id="user-name" name="user-name" placeholder="Enter User Name"/><br/>
+                <Label for="first-name">First Name</Label><br/>
+                <Input type="text" id="first-name" name="first-name" /><br/>
+                <Label for="last-name">Last Name:</Label><br/>
+                <Input type="text" id="last-name" name="last-name" /><br/>
+                <Label for="birthday">Birthday</Label><br/>
+                <Input type="date" id="birthday" name="birthday" /><br/>
+                <Button >Next</Button>
+            </Form>
 
             <Link to="/">
-                <button>enter</button>
+                <Button>enter</Button>
             </Link>
         </div>
     )
